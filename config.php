6c4f3508ccc2cd39442a35fd96f30e7c3f03cf49
Vipin <tanu.staplelogicin@gmail.com>
2022-03-25 19:05:49 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,13 @@
 <?PHP
 
-$dbuser = 'root';
-$dbpass = '';
-$dbname = 'web_survey'; 
+// $dbuser = 'root';
+// $dbpass = '';
+// $dbname = 'web_survey'; 
+// $dbhost = 'localhost';
+
+$dbuser = 'web-survey';
+$dbpass = '4H3-^JxWy&-%273';
+$dbname = 'web-survey'; 
 $dbhost = 'localhost';
 
 $mysqli = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
